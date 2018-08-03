9fb674a91fafbac36d062c3104df2a75d150e635
longvubka98 <33171335+longvubka98@users.noreply.github.com>
2018-08-03 10:28:35 +0700

    ok

--- js/js.js
+++ js/js.js
@@ -18,30 +18,46 @@ $(window).on('scroll',function() {
 
 
 
+$(document).ready(function(){
+	$('.customer-logos').slick({
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 1000,
+		arrows: false,
+		dots: false,
+		pauseOnHover: false,
+		responsive: [{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 4
+			}
+		}, {
+			breakpoint: 520,
+			settings: {
+				slidesToShow: 3
+			}
+		}]
+	});
+});
 
+new WOW().init();
 
-
-
-
-		$(document).ready(function(){
-			$('.customer-logos').slick({
-				slidesToShow: 6,
-				slidesToScroll: 1,
-				autoplay: true,
-				autoplaySpeed: 1000,
-				arrows: false,
-				dots: false,
-				pauseOnHover: false,
-				responsive: [{
-					breakpoint: 768,
-					settings: {
-						slidesToShow: 4
-					}
-				}, {
-					breakpoint: 520,
-					settings: {
-						slidesToShow: 3
-					}
-				}]
+var a=0;
+$(window).scroll(function(){
+	var oTop = $('#nav').offset().top - window.innerHeight;
+	if (a == 0 && $(window).scrollTop() > oTop){
+		$('.count').each(function () {
+			$(this).prop('Counter',0).animate({
+				Counter: $(this).text()
+			}, {
+				duration: 4000,
+				easing: 'swing',
+				step: function (now) {
+					$(this).text(Math.ceil(now));
+				}
 			});
 		});
+		a=1;
+	}
+});
